06f2906655e3cf2ddf285a7ea6323ad84d701d21
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-13 19:36:11 +0000

    automatically backed up by learn

--- db/migrate/007_fix_column_name_haunted_houses.rb
+++ db/migrate/007_fix_column_name_haunted_houses.rb
@@ -1,7 +1,7 @@
-class FixColumnNameHauntedHouses < ActiveRecord::Migration
+class FixColumnNameHauntedHouses < ActiveRecord::Migration[5.1]
 
   def change
     rename_column :haunted_houses, :long_descriptiong, :description
   end
 
-end 
+end
